3f8917a3ede89cb3c3b001ac38085589d8f9b777
sixiaoyou <yunshaliying@sina.com>
2017-06-18 17:32:13 +0800

    Init

--- Matplotlib/matplotlib1.py
+++ Matplotlib/matplotlib1.py
@@ -1,11 +1,130 @@
 #--coding:utf-8--
-
+import numpy as np
 import matplotlib.pyplot as plt
 
-year = [1950,1970,1990,2010]
-pop = [2.519,3.692,5.263,6.972]
 #折线图
+# year = [1950,1970,1990,2010]
+# pop = [2.519,3.692,5.263,6.972]
 # plt.plot(year,pop)
+
 #散点图
-plt.scatter(year,pop)
-plt.show()
+# n = 1024
+# X = np.random.normal(0,1,n)
+# Y = np.random.normal(0,1,n)
+# T = np.arctan2(Y,X) #for color value
+# # plt.scatter(X,Y,s = 75,c = T,alpha = 0.5)
+# plt.scatter(np.arange(5),np.arange(5))
+# plt.xlim((-1.5,1.5))
+# plt.ylim((-1.5,1.5))
+# plt.xticks(())
+# plt.yticks(())
+# plt.scatter(year,pop)
+
+#柱状图
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# n = 12
+# X = np.arange(n)
+# Y1 = (1 - X / float(n)) * np.random.uniform(0.5, 1.0, n)
+# Y2 = (1 - X / float(n)) * np.random.uniform(0.5, 1.0, n)
+#
+# plt.bar(X, +Y1,facecolor = '#9999ff',edgecolor = 'white')
+# plt.bar(X, -Y2,facecolor = '#ff9999',edgecolor = 'white')
+#
+# for x,y in zip(X,Y1):
+#     #ha: horizontal alignment
+#     plt.text(x + 0.4,y + 0.05,'%.2f'%y,ha = 'center',va = 'bottom')
+#
+# for x,y in zip(X,Y2):
+#     #ha: horizontal alignment
+#     plt.text(x + 0.4,-y - 0.05,'-%.2f'%y,ha = 'center',va = 'top')
+#
+#
+# plt.xlim(-.5, n)
+# plt.xticks(())
+# plt.ylim(-1.25, 1.25)
+# plt.yticks(())
+
+#等高线图
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# def f(x,y):
+#     # the height function
+#     return (1 - x / 2 + x**5 + y**3) * np.exp(-x**2 -y**2)
+#
+# n = 256
+# x = np.linspace(-3, 3, n)
+# y = np.linspace(-3, 3, n)
+# #将x,y绑定成网格的输入值
+# X,Y = np.meshgrid(x, y)
+# plt.contourf(X,Y,f(X,Y),8,alpha = 0.75,cmap = plt.cm.hot)
+#
+# C = plt.contour(X,Y,f(X,Y),8,colors = 'black',linewidth = .5)
+# plt.clabel(C,inline = True,fontsize = 10)
+# plt.xticks(())
+# plt.yticks(())
+# plt.show()
+#image
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# a = np.array([0.313660827978, 0.365348418405, 0.423733120134,
+#               0.365348418405, 0.439599930621, 0.525083754405,
+#               0.423733120134, 0.525083754405, 0.651536351379]).reshape(3,3)
+# plt.imshow(a,interpolation = 'nearest',cmap = 'bone',origin = 'upper')
+# plt.colorbar(shrink = 0.9)
+#
+# plt.xticks(())
+# plt.yticks(())
+#3D image
+# from mpl_toolkits.mplot3d import Axes3D
+# fig = plt.figure()
+# ax = Axes3D(fig)
+# # X, Y value
+# X = np.arange(-4, 4, 0.25)
+# Y = np.arange(-4, 4, 0.25)
+# X, Y = np.meshgrid(X, Y)    # x-y 平面的网格
+# R = np.sqrt(X ** 2 + Y ** 2)
+# # height value
+# Z = np.sin(R)
+#
+# ax.plot_surface(X,Y,Z,rstride = 1,cstride = 1,cmap = plt.get_cmap('rainbow'))
+# ax.contourf(X,Y,Z,zdic = 'z',offset = -4,cmap = 'rainbow')
+# ax.set_zlim(-2,2)
+# plt.show()
+
+#subplot
+# plt.figure()
+# plt.subplot(2,1,1)
+# plt.plot([0,1],[0,1])
+# plt.subplot(2,3,4)
+# plt.plot([0,1],[0,2])
+# plt.subplot(235)
+# plt.plot([0,1],[0,3])
+# plt.subplot(236)
+# plt.plot([0,1],[0,4])
+import matplotlib.gridspec as gridspec
+##method1: subplot2grid
+# plt.figure()
+# ax1 =plt.subplot2grid((3,3),(0,0),colspan = 3,rowspan  = 1)
+# ax1.plot([1,2],[1,2])
+# ax1.set_title('ax1_title')
+# ax2 =plt.subplot2grid((3,3),(1,0),colspan = 2)
+# ax3 =plt.subplot2grid((3,3),(1,2),rowspan = 2)
+# ax4 =plt.subplot2grid((3,3),(2,0))
+# ax5 =plt.subplot2grid((3,3),(2,1))
+##method2: gridspec
+# plt.figure()
+# gs = gridspec.GridSpec(3,3)
+# ax6 = plt.subplot(gs[0,:])
+# ax7 = plt.subplot(gs[1,:2])
+# ax8 = plt.subplot(gs[1:,2])
+# ax9 = plt.subplot(gs[-1,0])
+# ax10 = plt.subplot(gs[-1,-2])
+##method3: easy to define structure
+# f,((ax11,ax12),(ax21,ax22)) = plt.subplots(2,2,sharex= True,sharey = True)
+# ax11.scatter([1,2],[1,2])
+# plt.tight_layout()
+# plt.show()
--- Matplotlib/matplotlib6.py
+++ Matplotlib/matplotlib6.py
@@ -11,29 +11,111 @@ import numpy as np
 #____________________________________________________________________
 #2 Figure
 #一个Figure对象可以包含多个子图(Axes)
-x = np.linspace(-1,1,50)
-y1 = 2 * x + 1
-y2 = x**2
-plt.figure()
-plt.plot(x,y1)
-#指定figure窗口号为3，窗口长宽分别为8,5
-plt.figure(num=3,figsize = (8,5))
-plt.plot(x,y2)
-#color指定线的颜色,linewidth指定线宽,linesyle指定线的类型
-plt.plot(x,y1,color = 'red',linewidth = 1.0,linestyle = '--')
-#设置坐标轴的范围
-plt.xlim((-1,2))
-plt.ylim((-2,3))
-#描述x轴
-plt.xlabel('I am x')
-#描述y轴
-plt.ylabel('I am y')
+# x = np.linspace(-1,1,50)
+# y1 = 2 * x + 1
+# y2 = x**2
+# plt.figure()
+# plt.plot(x,y1)
+# #指定figure窗口号为3，窗口长宽分别为8,5
+# plt.figure(num=3,figsize = (8,5))
+# plt.plot(x,y2)
+# #color指定线的颜色,linewidth指定线宽,linesyle指定线的类型
+# plt.plot(x,y1,color = 'red',linewidth = 1.0,linestyle = '--')
+# #设置坐标轴的范围
+# plt.xlim((-1,2))
+# plt.ylim((-2,3))
+# #描述x轴
+# plt.xlabel('I am x')
+# #描述y轴
+# plt.ylabel('I am y')
+#
+# new_ticks = np.linspace(-1,2,5)
+# print(new_ticks)
+# #xticks 设置x轴间隔
+# plt.xticks(new_ticks)
+# #yticks将y轴坐标转换成指定内容
+# #通过 r'$...$' 改变字体,通过转义符可以将\alpha转换成α
+# plt.yticks([-2,-1.8,-1,1.22,3,],[r'$really\ bad$',r'$bad\ \alpha$',r'$normal$',r'$good$',r'$really\ good$'])
+#
+# #gca = 'get current axis'获取当前axes
+# ax = plt.gca()
+# #消除右边的轴以及上边的轴
+# ax.spines['right'].set_color('none')
+# ax.spines['top'].set_color('none')
+# #将x轴和y轴移动至0刻度处
+# ax.xaxis.set_ticks_position('bottom')
+# ax.yaxis.set_ticks_position('left')
+# ax.spines['bottom'].set_position(('data',0))#outward,axes
+# ax.spines['left'].set_position(('data',0))
+# plt.show()
+#____________________________________________________________________
+#3 Legend 图例
+# x = np.linspace(-1,1,50)
+# y1 = 2 * x + 1
+# y2 = x**2
+# plt.figure()
+#
+# #设置坐标轴的范围
+# plt.xlim((-1,2))
+# plt.ylim((-2,3))
+# #描述x轴
+# plt.xlabel('I am x')
+# #描述y轴
+# plt.ylabel('I am y')
+#
+# new_ticks = np.linspace(-1,2,5)
+# print(new_ticks)
+# #xticks 设置x轴间隔
+# plt.xticks(new_ticks)
+# #yticks将y轴坐标转换成指定内容
+# #通过 r'$...$' 改变字体,通过转义符可以将\alpha转换成α
+# plt.yticks([-2,-1.8,-1,1.22,3,],[r'$really\ bad$',r'$bad\ \alpha$',r'$normal$',r'$good$',r'$really\ good$'])
+#
+# l1, = plt.plot(x,y2,label = 'up')
+# #color指定线的颜色,linewidth指定线宽,linesyle指定线的类型
+# l2, = plt.plot(x,y1,color = 'red',linewidth = 1.0,linestyle = '--',label = 'down')
+# #三个最重要的必要参数
+# # parent --- legend的父artist， 包含legend的对象
+# #        比如用ax.legend()调用之后
+# #        >>> print ax.get_legend().parent
+# #        Axes(0.125,0.1;0.775x0.8)
+# # handles --- 图例上面画出的各个artist（lines, patches）
+# # labels --- artist 对应的标签
+# #handle:The original object which is used to generate an appropriate entry in the legend.
+# plt.legend(handles = [l1,l2,],labels = ['aaa','bbb'] ,loc = 'best')
+#
+# plt.show()
+#____________________________________________________________________
+#4 Annotation 注解
+import matplotlib.pyplot as plt
+import numpy as np
 
-new_ticks = np.linspace(-1,2,5)
-print(new_ticks)
-#xticks 设置x轴间隔
-plt.xticks(new_ticks)
-#yticks将y轴坐标转换成指定内容
-#通过 r'$...$' 改变字体,通过转义符可以将\alpha转换成α
-plt.yticks([-2,-1.8,-1,1.22,3,],[r'$really\ bad$',r'$bad\ \alpha$',r'$normal$',r'$good$',r'$really\ good$'])
+x = np.linspace(-3, 3, 50)
+y = 2*x + 1
+
+plt.figure(num=1, figsize=(8, 5),)
+plt.plot(x, y,)
+
+ax = plt.gca()
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.spines['bottom'].set_position(('data', 0))
+ax.yaxis.set_ticks_position('left')
+ax.spines['left'].set_position(('data', 0))
+
+x0 = 1
+y0 = 2*x0 + 1
+plt.scatter(x0,y0,s=50,color = 'b')
+plt.plot([x0,x0],[y0,0],'k--',lw = 2.5)
+
+#method 1
+#################
+#method 1
+plt.annotate(r'$2x+1=%s$'%y0,xy=(x0,y0),xycoords = 'data',xytext = (+30,-30),textcoords = 'offset points',
+             fontsize = 16,arrowprops = dict(arrowstyle = '->',connectionstyle = 'arc3,rad = .2'))
+
+#method 2
+plt.text(-3.7,3,r'$This\ is\ the\ some\ test.\ \mu\ \sigma_i\ \alpha_t$',fontdict = {'size':16,'color':'r'})
 plt.show()
